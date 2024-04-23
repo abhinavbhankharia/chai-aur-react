86142847cc8f8b348d93f7bca0c4428243bba383
abhinavbhankharia <abhinavbhankharia@gmail.com>
2024-04-24 01:27:05 +0530

    custom hook

--- 06currencyconvertor/src/App.js
+++ 06currencyconvertor/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <h1 classname="text-3xl bg-orange-500">Currrency App</h1>
+    <h1 className= "text-3xl bg-orange-500" >Currrency App</h1>
     </>
   );
 }
