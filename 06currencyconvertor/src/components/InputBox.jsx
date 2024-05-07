d85bd3f86737fca7d8a03422d0290e977a99bd86
abhinavbhankharia <abhinavbhankharia@gmail.com>
2024-05-08 00:10:27 +0530

    adding functionality for currency project

--- 06currencyconvertor/src/components/InputBox.jsx
+++ 06currencyconvertor/src/components/InputBox.jsx
@@ -1,34 +1,35 @@
+import { useId } from "react";
 import React from 'react'
 
 function InputBox({     //takes two input from user label and className
     label,
     amount,
-    onAmmountChange,
-    onCurrenyChange,
-    currenyOptions = [],
+    onAmountChange,
+    onCurrencyChange,
+    currencyOption = [],
     selectCurrency = "usd",
-    amoountDisable = false,
+    amountDisable = false,
     currencyDisable = false,
     className = "",
 }) {
-   
+   const ammountInputId = useId()
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}` }>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor="ammounInputId" className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={ammountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled={amoountDisable}
+                    disabled={amountDisable}
                     value={amount}
-                    onChange={(e) => onAmmountChange && 
-                    onAmmountChange(Number(e.target.value))}
-
+                    onChange={(e) => onAmountChange &&      //checks for change of amount
+                    onAmountChange(Number(e.target.value))} //checks if onAmountchnage exists or not 
+                    
 
                 />
             </div>
@@ -36,13 +37,14 @@ function InputBox({     //takes two input from user label and className
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value={selectCurrency}
-                    onChange={(e)=>onAmmountChange && onAmmountChange(Number(e.target.value))}
+                    value={selectCurrency}                  //used for picking up currency type
+                    onChange={(e)=>onCurrencyChange &&        //checks for change of amount
+                    onCurrencyChange(Number(e.target.value))}      
                     disabled={currencyDisable}
 
                     >
-                    {currencyOptions.map((currency)=>(
-                        <option key={currency} value={currency}>
+                    {currencyOption.map((currency)=>(              //key needs to be passed while using loops in JSX
+                        <option key={currency} value={currency}>    
                         {currency}
                         </option>
                     ))}
